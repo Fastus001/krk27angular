23ee7f3fa8ff040e903c78d5b0b98cd6df747ac1
Fastus001 <tkarczmarzyk@gmail.com>
2021-03-28 11:20:16 +0200

    Mock server creation.

---
+++ tools/mock-server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const port = 3200;
+
+const server = http.createServer( (req, res) => {
+  res.statusCode = 200;
+  // res.setHeader('Content-type', 'text/plain');
+  res.setHeader('Content-type', 'application/json');
+  res.end(`
+  [
+  {"id": 1,
+   "name": "Pfizer",
+    details:{
+  "price": 200,"country": "us"
+  },
+  "id": 2,
+   "name": "NovaVax",
+    details:{
+  "price": 220,"country": "gb"
+  }
+  ]`);
+});
+
+server.listen(port, '127.0.0.1', () => {
+  console.log('Serwer wystartowal na porcie' + port);
+});
